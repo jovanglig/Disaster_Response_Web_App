6b71d9c1702ba2a6e1b55d54f16ac03e76aba94c
Jovan <jovan@alwaysbelearning.com>
2020-11-24 12:29:38 +0100

    second commit

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request
 from plotly.graph_objs import Bar, Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
